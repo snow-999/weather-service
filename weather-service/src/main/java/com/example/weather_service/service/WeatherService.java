364c6abf5ba8e6ca9a8f96cafec86c884a34b6bf
khaled <khaledleader012@gmail.com>
2025-08-24 09:11:50 +0300

    add get and save current weather method

--- weather-service/src/main/java/com/example/weather_service/service/WeatherService.java
+++ weather-service/src/main/java/com/example/weather_service/service/WeatherService.java
@@ -1,8 +1,29 @@
 package com.example.weather_service.service;
 
+import com.example.weather_service.converter.WeatherConverter;
+import com.example.weather_service.dto.WeatherDTO;
+import com.example.weather_service.entity.WeatherEntity;
+import com.example.weather_service.repository.WeatherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WeatherService {
+    @Autowired
+    private  WeatherRepository weatherRepository;
+    @Autowired
+    private WeatherConverter weatherConverter;
 
+    public List<WeatherDTO> getCurrentWeather() {
+        List<WeatherEntity> weatherEntityList = weatherRepository.findAll();
+        return weatherEntityList.stream().map(weatherConverter::convertToDTO).toList();
+    }
+
+    public WeatherDTO saveCurrentWeather (WeatherDTO weatherDTO) {
+        WeatherEntity weatherEntity = weatherConverter.convertDTOToEntity(weatherDTO);
+        weatherEntity = weatherRepository.save(weatherEntity);
+        return  weatherConverter.convertToDTO(weatherEntity);;
+    }
 }
